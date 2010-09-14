31e9f1076749fd04e14e5e2124bf14c0d7384a74
Joshua Peek <josh@joshpeek.com>
2010-09-14 18:06:44 -0500

    Rename server to process

--- examples/server.js
+++ examples/server.js
@@ -1,12 +1,10 @@
-// ./bin/nackup -f test/nack.sock examples/config.ru
-
 require.paths.unshift(__dirname + "/../lib");
 
-var http   = require('http');
-var server = require('nack/server');
+var http    = require('http');
+var process = require('nack/process');
 
-var app = server.createServer(__dirname + "/config.ru");
+var app = process.createProcess(__dirname + "/config.ru");
 
 http.createServer(function (req, res) {
-  app.request(req, res);
+  app.proxyRequest(req, res);
 }).listen(8124, "127.0.0.1");
